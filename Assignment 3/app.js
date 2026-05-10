c7ffa47d81927c2c222178527513b77dfbf88bfc
Ayan Khan <aw1800606@gmail.com>
2026-05-10 18:57:20 +0500

    Assignment 3 Problem Questions Solve
    Solved first 10 assignment questions

---
+++ Assignment 3/app.js
@@ -0,0 +1,48 @@
+// Global Scope 
+// var abc = "Ayan Khan"
+// let def = "Ayan Khan"
+// const xyz = "Ayan Khan"
+
+// console.log(abc,def,xyz); GLOBAL
+
+
+// function abc(){
+//     var na = 'Ayan Khan'
+//     let str = 'Ayan Khan'
+//     const kk = 'Ayan Khan'
+// }
+// abc()
+// console.log(na,str,kk); 
+// Reason var Function Scoped Let and Const Block Scope
+
+
+// if(true){
+//     var a = 12
+// }
+// console.log(a);
+
+// if(true){
+//     let a = 12
+// }
+// console.log(a);
+
+// if(true){
+//     const a = 12
+// }
+// console.log(a); Beacuse of Hoisting
+
+// console.log(na);
+// let na = "Ayan Khan"
+
+// console.log(na);
+// var na = "Ayan Khan"
+
+// var na = "Ayan Khan"
+// var na = "Ayan Khan" var redecalre hoskta hai
+
+// let na = "Ayan Khan"
+// let na = "Ayan Khan" // Don't Redecalred but reInitialized
+
+// const na = "Ayan Khan"
+// const na = "Ayan Khan" // Don't Redecalred and not Reinitialize
+
